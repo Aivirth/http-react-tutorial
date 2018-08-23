1dc6b9d8879e71573e0ea6f45dca2e8d33dc1095
Aivirth <aivirth.work@gmail.com>
2018-08-23 17:53:14 +0200

    Added router base url

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router basename="/my-app">
         <div className="App">
           <Blog />
         </div>
